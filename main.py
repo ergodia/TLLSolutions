b6a381ba6f33b85875723dc0e551cadc57be162d
ergodia <lionplanes12@gmail.com>
2022-01-12 13:39:04 +0100

    Holland graph aangemaakt

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ sys.path.append(os.path.join(PATH, "code", "classes"))
 sys.path.append(os.path.join(PATH, "code", "trials"))
 
 
-
 # import of the needed fuctions
 from connections import Connections
 from stations import Stations
 from graph import holland_graph
+
 
 def main():
     # TODO Moet misschien via een terminal ARGV worden gedaan zodat je daar kan aangeven welke bestanden moeten worden gebruikt. (Tim)
