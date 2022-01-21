af7a1f5fcabf11db34a1d713ba6a667c7d7156ee
LFloor <lienekefloor@icloud.com>
2022-01-21 13:57:08 +0100

    Verhouding van de kaart juist gemaakt (met figsize)

--- codes/trials/graph.py
+++ codes/trials/graph.py
@@ -26,23 +26,30 @@ def holland_graph(path, stations_dict: dict, bbox):
     # map = plt.imread(path / "data" / "holland.png")
     map = plt.imread(path / "data" / "Kaart_NL_grijs.png")
 
-    fig, ax = plt.subplots()  
+    fig, ax = plt.subplots(figsize=(32, 40))  
 
     for key in stations_dict:
-        ax.plot(stations_dict[key].y, stations_dict[key].x, zorder=1, linewidth=3, marker="o", label=key)
+        ax.plot(stations_dict[key].y, stations_dict[key].x, zorder=1, linewidth=6, marker="o",  markersize=20, label=key)
+
+    #print(stations_dict['train 0'].index)
 
     # Plek waarin lijnen geplot moeten worden
     ax.set_xlim(bbox[0], bbox[1])
     ax.set_ylim(bbox[2], bbox[3])
     
-    ax.imshow(map, zorder=0, extent = bbox, aspect="equal") # kaart inladen zorder(onder boven)
-    fig.set_size_inches(10, 40, forward=True) #kaart formaat
+    ax.imshow(map, zorder=0, extent = bbox, aspect="auto") # kaart inladen order(onder boven)
+    #fig.set_size_inches(10, 40, forward=True) #kaart formaat
 
-    # legenda weergave
-    ax.legend()
-        
+    # legenda weergave 
+    # https://stackoverflow.com/questions/7125009/how-to-change-legend-size-with-matplotlib-pyplot
+    ax.legend(loc=2, prop={'size': 30})
+
+
     plt.axis("off")
-    plt.title("Holland Graph")
+    plt.title("Holland Graph", fontsize=60)
     
     # fig.savefig(path / "data" / "holland_graph.png", bbox_inches="tight", dpi=350)
-    fig.savefig(path / "data" / "Kaart_NL_grijs_graph.png", bbox_inches="tight", dpi=350)
+    fig.savefig(path / "data" / "Kaart_NL_grijs_graph.png", bbox_inches="tight", dpi=150)
+
+
+# https://matplotlib.org/basemap/users/examples.html
