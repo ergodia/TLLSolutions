a14a86fd7e4eaeeafe837d1b0c7312d2a3ce138e
LFloor <lienekefloor@icloud.com>
2022-01-24 11:07:19 +0100

    Markers bij de treinstations geplaatst

--- codes/trials/graph.py
+++ codes/trials/graph.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+import geopandas as gpd
 import numpy as np
+import plotly.graph_objects as go
 
 
 # def holland_graph_1st(path, stations):
@@ -26,12 +29,35 @@ def holland_graph(path, stations_dict: dict, bbox):
     # map = plt.imread(path / "data" / "holland.png")
     map = plt.imread(path / "data" / "Kaart_NL_grijs.png")
 
-    fig, ax = plt.subplots(figsize=(32, 40))  
+    fig, ax = plt.subplots(figsize=(17, 20))  
 
+    # plot line and markers
     for key in stations_dict:
-        ax.plot(stations_dict[key].y, stations_dict[key].x, zorder=1, linewidth=6, marker="o",  markersize=20, label=key)
+        ax.plot(stations_dict[key].y,
+            stations_dict[key].x,
+            zorder=1,
+            linewidth=5,
+            marker="o",
+            markersize=17,
+            markeredgewidth=4,
+            markerfacecolor='w',
+            label=key)
 
-    #print(stations_dict['train 0'].index)
+        print(stations_dict[key].index)
+        
+    #https://queirozf.com/entries/add-labels-and-text-to-matplotlib-plots-annotation-examples
+    for x,y in zip(stations_dict[key].y,stations_dict[key].x):
+        stations=stations_dict[key].index
+        for i in stations:
+            ax.annotate(i, # this is the text
+                (x,y), # these are the coordinates to position the label
+                textcoords="offset points", # how to position the text
+                xytext=(0,15), # distance from text to points (x,y)
+                ha='center',
+                fontsize=10,
+                bbox=dict(boxstyle="round", fc="w", alpha=0.5)) # horizontal alignment can be left
+
+    #print(stations_dict[key].index)
 
     # Plek waarin lijnen geplot moeten worden
     ax.set_xlim(bbox[0], bbox[1])
@@ -40,10 +66,12 @@ def holland_graph(path, stations_dict: dict, bbox):
     ax.imshow(map, zorder=0, extent = bbox, aspect="auto") # kaart inladen order(onder boven)
     #fig.set_size_inches(10, 40, forward=True) #kaart formaat
 
-    # legenda weergave 
-    # https://stackoverflow.com/questions/7125009/how-to-change-legend-size-with-matplotlib-pyplot
+    # show legend 
     ax.legend(loc=2, prop={'size': 30})
-
+    
+    # x = 5.0
+    # y = 3.0
+    # ax.annotate(stations_dict[key].index[0], x, y)
 
     plt.axis("off")
     plt.title("Holland Graph", fontsize=60)
@@ -53,3 +81,4 @@ def holland_graph(path, stations_dict: dict, bbox):
 
 
 # https://matplotlib.org/basemap/users/examples.html
+# https://plotly.com/python/lines-on-maps/
