cc8f35113e6a2dd4b9ea71bfa85d0218b2c36023
Lepel1998 <luca-nederhorst@hotmail.nl>
2022-02-01 11:29:15 +0100

    traveling_salesman_dis.py

---
+++ codes/analyze/traveling_salesman_dis.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from ..algorithms.traveling_salesman_rail import Traveling_Salesman_Rail
+from ..classes.network import Network
+from ..classes.graph import Graph
+from ..calculations.line_quality import K, load_connections
+from pathlib import Path
+from progress.spinner import Spinner
+
+PATH = Path(os.path.dirname(os.path.realpath(__file__))).parents[1]
+SCORE_CONNECTIONS = load_connections(PATH / "data" / "ConnectiesNationaal.csv")
+
+def traveling_salesman_score(iterations, algorithm_iterations, temperature):
+    graph = Graph(PATH / "data" / "StationsNationaal.csv", PATH / "data" / "ConnectiesNationaal.csv")
+    base_network = Network(PATH / "data" / "output_nat.csv", graph.stations)
+    
+    scores = {}
+    
+    spinner = Spinner('Running')
+    for iteration in range(iterations):
+        network, score = Traveling_Salesman_Rail(base_network, 180, 20, algorithm_iterations, temperature, SCORE_CONNECTIONS).run()
+        
+        if score in scores:
+            scores[score] += 1
+        else:
+            scores[score] = 1
+        spinner.next()
+    
+    data = pd.Series(scores)
+    ax = data.plot.bar(x="score", y="amount")
+    plt.show()
+    
+    
--- main_experiment.py
+++ main_experiment.py
@@ -1,7 +1,9 @@
 from codes.analyze.simulated_annealing_dis import simulated_annealing_score
+from codes.analyze.traveling_salesman_dis import traveling_salesman_score
 
 def main():
     simulated_annealing_score(100, 100, 20)
+    traveling_salesman_score(100, 100, 20)
 
 if __name__ == "__main__":
     main()
