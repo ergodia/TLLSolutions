bc952e0ab84aa482c41b56af13b28bf89a1e1718
ergodia <lionplanes12@gmail.com>
2022-02-01 11:32:19 +0100

    Conflicten opgelost

--- main_experiment.py
+++ main_experiment.py
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 from codes.analyze.simulated_annealing_dis import simulated_annealing_score, simulated_annealing_score_ot
+from codes.analyze.traveling_salesman_dis import traveling_salesman_score
 
 def main():
     # simulated_annealing_score(100, 100, 20)
     simulated_annealing_score_ot(50000, 20)
-=======
-from codes.analyze.simulated_annealing_dis import simulated_annealing_score
-from codes.analyze.traveling_salesman_dis import traveling_salesman_score
-
-def main():
-    simulated_annealing_score(100, 100, 20)
     traveling_salesman_score(100, 100, 20)
->>>>>>> cc8f35113e6a2dd4b9ea71bfa85d0218b2c36023
 
 if __name__ == "__main__":
     main()
